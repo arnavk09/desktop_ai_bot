e247cf8d102bfa845bace004dd74c85a9f134122
Arnav Katgeri <arnav02@outlook.com>
2022-03-03 16:01:54 +0530

    takeCommand() refined

--- jarvis.py
+++ jarvis.py
@@ -1,12 +1,19 @@
 import pyttsx3
 import speech_recognition as sr
 import datetime as dt
-
+from datetime import datetime
 
 engine = pyttsx3.init('sapi5')
 voices = engine.getProperty('voices')
 
 engine.setProperty('voice', voices[2].id)
+
+
+def timeNow():
+    now = datetime.now()
+    ct = now.strftime("%H:%M:%S")
+    print(type(ct))
+    print("Current Time =", ct)
 
 
 def speak(audio):
@@ -18,13 +25,15 @@ def wishMe():
     '''announcer!'''
     hour = int(dt.datetime.now().hour)
     if hour >= 0 and hour < 6:
-        speak("It's quite late in the day. What do you want me to do?")
+
+        speak("It's quite late in the day. What do you want me to do? ")
     elif hour >= 6 and hour < 12:
         speak("Good morning!")
     elif hour >= 12 and hour < 18:
-        speak("Good afternoon! What do you want me to do?")
+        speak("Good afternoon! What do you want me to do? ")
     elif hour >= 18 and hour < 24:
         speak("Good evening! What do you want me to do?")
+
 
 def takeCommand():
     '''Takes a command from user microphone and returns string output'''
@@ -34,9 +43,9 @@ def takeCommand():
         r.pause_threshold = 1
         audio = r.listen(source)
     try:
-        print("recognizing...")
+        print("Recognizing...")
         query = r.recognize_google(audio, language='en-in')
-        print(f"user said: {query}\n")
+        print(f"User said: {query}\n")
     except Exception as e:
         print(e)
         speak("Try saying it again!")
@@ -46,4 +55,5 @@ def takeCommand():
 
 if __name__ == "__main__":
     wishMe()
-    takeCommand()
+    timeNow()
+    # takeCommand()
